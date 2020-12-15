313da1e68674e3f3288f2ddd84e6de3ff595a07a
Bartosz Trybus <trub@wp.pl>
2020-12-15 10:08:35 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace ProducentKonsumentC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.m_Pdelay = new System.Windows.Forms.TextBox();
+            this.m_Kdelay = new System.Windows.Forms.TextBox();
+            this.m_Pcounter = new System.Windows.Forms.TextBox();
+            this.m_Kcounter = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.m_wBuforze = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 43);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Produkuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(237, 43);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Konsumuj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(72, 230);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(264, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // m_Pdelay
+            // 
+            this.m_Pdelay.Location = new System.Drawing.Point(152, 84);
+            this.m_Pdelay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.m_Pdelay.Name = "m_Pdelay";
+            this.m_Pdelay.Size = new System.Drawing.Size(56, 22);
+            this.m_Pdelay.TabIndex = 3;
+            this.m_Pdelay.Text = "500";
+            this.m_Pdelay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.m_Pdelay.TextChanged += new System.EventHandler(this.m_Pdelay_TextChanged);
+            // 
+            // m_Kdelay
+            // 
+            this.m_Kdelay.Location = new System.Drawing.Point(253, 84);
+            this.m_Kdelay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.m_Kdelay.Name = "m_Kdelay";
+            this.m_Kdelay.Size = new System.Drawing.Size(56, 22);
+            this.m_Kdelay.TabIndex = 4;
+            this.m_Kdelay.Text = "1000";
+            this.m_Kdelay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.m_Kdelay.TextChanged += new System.EventHandler(this.m_Pdelay_TextChanged);
+            // 
+            // m_Pcounter
+            // 
+            this.m_Pcounter.Location = new System.Drawing.Point(253, 129);
+            this.m_Pcounter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.m_Pcounter.Name = "m_Pcounter";
+            this.m_Pcounter.Size = new System.Drawing.Size(56, 22);
+            this.m_Pcounter.TabIndex = 5;
+            this.m_Pcounter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // m_Kcounter
+            // 
+            this.m_Kcounter.Location = new System.Drawing.Point(253, 161);
+            this.m_Kcounter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.m_Kcounter.Name = "m_Kcounter";
+            this.m_Kcounter.Size = new System.Drawing.Size(56, 22);
+            this.m_Kcounter.TabIndex = 6;
+            this.m_Kcounter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(215, 268);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Zatrzymaj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(47, 90);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Czas pracy:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(111, 129);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Wyprodukowano:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(113, 161);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Skonsumowano:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(12, 236);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Bufor:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(24, 11);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(251, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Problem producenta i konsumenta";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(159, 193);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "W buforze:";
+            // 
+            // m_wBuforze
+            // 
+            this.m_wBuforze.Location = new System.Drawing.Point(253, 193);
+            this.m_wBuforze.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.m_wBuforze.Name = "m_wBuforze";
+            this.m_wBuforze.Size = new System.Drawing.Size(56, 22);
+            this.m_wBuforze.TabIndex = 14;
+            this.m_wBuforze.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 318);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.m_wBuforze);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.m_Kcounter);
+            this.Controls.Add(this.m_Pcounter);
+            this.Controls.Add(this.m_Kdelay);
+            this.Controls.Add(this.m_Pdelay);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "ProducentKonsument";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox m_Pdelay;
+        private System.Windows.Forms.TextBox m_Kdelay;
+        private System.Windows.Forms.TextBox m_Pcounter;
+        private System.Windows.Forms.TextBox m_Kcounter;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox m_wBuforze;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProducentKonsumentC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const int N = 10;
+
+        private static int[] buffer = new int[N];                                               //buffor
+        private static int Pdelay = 500, Kdelay = 1000;                                         //opoznienie
+        private static int Pcounter = 0, Kcounter = 0;                                          //licznik produkcji/konsumpcji
+        private static int Bcounter = 0;
+            
+        private Thread tp;                                                               //watek producenta
+        private Thread tk;                                                               //watek konsumenta
+            
+        bool tpIsAlive = false;
+        bool tkIsAlive = false;
+
+        //producent
+        private static void Producent(object num)
+        {
+            int p = 0;
+
+            while( true )
+            {
+                Thread.Sleep(Pdelay);
+
+                buffer[p] = 1;
+                p = (p + 1) % N;
+                Pcounter++;
+                Bcounter++;
+
+            }
+        }
+
+        //konsument
+        private static void Konsument(object num)
+        {
+            int k = 0;
+
+            while (true)
+            {
+
+                buffer[k] = 0;
+                k = (k + 1) % N;
+                Kcounter++;
+                Bcounter--;
+
+                Thread.Sleep( Kdelay );
+            }
+        }
+
+        //wlaczenie producenta
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pdelay = int.Parse(m_Pdelay.Text);
+
+            tp = new Thread(Producent);
+            tp.Start();
+            tp.Name = "Producent";
+            tpIsAlive = true;
+
+            button1.Enabled = false;
+        }
+        
+        //wlaczenie konsumenta
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Kdelay = int.Parse(m_Kdelay.Text);
+
+            tk = new Thread(Konsument);
+            tk.Start();
+            tk.Name = "Konsument";
+            tkIsAlive = true;
+            
+            button2.Enabled = false;
+        }
+
+        private void m_Pdelay_TextChanged(object sender, EventArgs e)
+        {
+            Pdelay = int.Parse(m_Pdelay.Text);
+            Kdelay = int.Parse(m_Kdelay.Text);
+        }
+
+        //OnTimer
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string temp;
+            string temp2 = null;
+
+            for (int i = 0; i < N; i++)
+            {
+                temp = buffer[i] + "  |  ";
+                temp2 += temp;
+            }
+            textBox1.Text = temp2;
+
+            m_Pcounter.Text = Pcounter.ToString();
+            m_Kcounter.Text = Kcounter.ToString();
+
+            m_wBuforze.Text = Bcounter.ToString();
+        }
+
+        //przycisk Zatrzymaj
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (tpIsAlive)
+            {
+                tp.Abort();
+            }
+
+            if (tkIsAlive)
+            {
+                tk.Abort();
+            }
+        }
+
+    }
+}
